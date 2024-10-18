2cd11f2a2ef3fbba0f86e14e402e656f8610d0a1
loimtran <loi.tran.dev@gmail.com>
2024-10-18 18:20:06 +0530

    refactored: updated DBconnect function

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,20 +1,27 @@
 import mongoose from "mongoose"
-import dotenv from "dotenv"
-
-// Load environment variables
-dotenv.config()
 
 // Define the connection function
-const DBconnect = () => {
-  mongoose
-    .connect(process.env.MONGODB_URL as string)
-    .then(() => console.log("DB Connected Successfully"))
-    .catch((error) => {
-      console.log("DB Connection Failed")
-      console.error(error)
-      process.exit(1)
-    })
+
+const DBconnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL as string)
+    console.log("DB Connected Successfully")
+  } catch (error) {
+    console.error("DB Connection Failed:", error)
+    process.exit(1)
+  }
 }
 
 // Export the connection function
 export { DBconnect }
+
+// const DBconnect = () => {
+//   mongoose
+//     .connect(process.env.MONGODB_URL as string)
+//     .then(() => console.log("DB Connected Successfully"))
+//     .catch((error) => {
+//       console.log("DB Connection Failed")
+//       console.error(error)
+//       process.exit(1)
+//     })
+// }
