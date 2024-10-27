3dd9b16310268eb2a77a9726ccac30bf3b4d36ea
loimtran <loi.tran.dev@gmail.com>
2024-10-28 04:33:05 +0530

    updated remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
       },
     ],
     deviceSizes: [320, 420, 768, 1024, 1200],
@@ -16,4 +15,5 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
   },
 }
+
 export default nextConfig
