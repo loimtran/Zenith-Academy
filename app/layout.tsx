e337bebd684a58489f4b469e48c21c3dfd5622b3
loimtran <loi.tran.dev@gmail.com>
2024-11-20 15:51:46 +0530

    added ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { Toaster } from "react-hot-toast"
 
 import { cn } from "@/lib/utils"
 
-// import { ThemeProvider } from "../components/ThemeProviders";
 import Footer from "../components/Common/Footer"
-import Navbar from "../components/Common/Navbar"
+import Navbar from "../components/Common/Navbar/Navbar"
+import { ThemeProvider } from "../components/ThemeProviders"
 
 const geistSans = localFont({
   src: "../public/fonts/GeistVF.woff",
@@ -44,26 +44,26 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head />
       <body
         className={cn(
-          "relative h-full antialiased",
+          "relative h-full antialiased max-w-[1500px] mx-auto",
           geistSans.variable,
           geistMono.variable
         )}
       >
-        {/* <ThemeProvider
+        <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
-        > */}
-        <Toaster />
-        <Navbar />
-        {children}
-        <Footer />
-        {/* </ThemeProvider> */}
+        >
+          <Toaster />
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   )
