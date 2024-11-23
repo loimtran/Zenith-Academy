8d464721bc8528c36c52e75c268d2654ba2ead6b
loimtran <loi.tran.dev@gmail.com>
2024-11-23 16:24:00 +0530

    updated getCatalogaPageData service

--- services/catalogService.ts
+++ services/catalogService.ts
@@ -1,17 +1,7 @@
-import { toast } from "react-hot-toast"
-
-import { apiConnector } from "../utils/apiConnector"
-import { catalogData } from "../utils/apis"
-
-interface ErrorResponse {
-  response?: {
-    data: any // You can be more specific here based on the API error response structure
-  }
-}
+import { apiConnector } from "@/utils/apiConnector"
+import { catalogData } from "@/utils/apis"
 
 export const getCatalogaPageData = async (categoryId: string | number) => {
-  // (setProgress(50)
-
   let result: any = []
 
   try {
@@ -32,17 +22,8 @@ export const getCatalogaPageData = async (categoryId: string | number) => {
     result = response.data
   } catch (error) {
     console.log("CATALOG PAGE DATA API ERROR....", error)
-    // Check if it's an ErrorResponse structure
-    if ((error as ErrorResponse).response?.data) {
-      toast.error("No Course added to this category yet")
-      result = (error as ErrorResponse).response?.data
-    } else {
-      toast.error("An unexpected error occurred in getCatalogaPageData")
-    }
+    result = (error as any)?.response?.data
   }
-
-  // toast.dismiss(toastId);
-  // (setProgress(100)
 
   return result
 }
