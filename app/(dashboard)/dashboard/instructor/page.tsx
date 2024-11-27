aa784053451ada38ffe0d271b8db0af5451b7f71
loimtran <loi.tran.dev@gmail.com>
2024-11-27 19:07:44 +0530

    protected InstructorDashboard with InstructorRoute

--- app/(dashboard)/dashboard/instructor/page.tsx
+++ app/(dashboard)/dashboard/instructor/page.tsx
@@ -1,5 +1,10 @@
-import InstructorDashboard from "../../../../components/Dashboard/Instructor/InstructorDashboard"
+import InstructorRoute from "@/components/Auth/InstructorRoute"
+import InstructorDashboard from "@/components/Dashboard/Instructor/InstructorDashboard"
 
 export default function InstructorDashboardPage() {
-  return <InstructorDashboard />
+  return (
+    <InstructorRoute>
+      <InstructorDashboard />
+    </InstructorRoute>
+  )
 }
