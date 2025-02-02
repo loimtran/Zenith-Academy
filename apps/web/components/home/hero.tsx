117c46d39380605ea9e18e06a209da374a9bbf5f
loimtran <loi.tran.dev@gmail.com>
2025-02-03 00:59:28 +0530

    feat: add Hero component with floating images, badges, and video background

---
+++ apps/web/components/home/hero.tsx
@@ -0,0 +1,175 @@
+import Link from "next/link"
+import { ChevronRight, User } from "lucide-react"
+import { motion } from "motion/react"
+
+import Floating, { FloatingElement } from "@/components/ui/parallax-floating"
+
+import { Button } from "../ui/button"
+import HeroBadge from "../ui/hero-badge"
+import { ButtonOutline, MainButton } from "../ui/theme-button"
+import HighlightText from "./highlighted-text"
+
+const exampleImages = [
+  {
+    url: "https://images.unsplash.com/photo-1727341554370-80e0fe9ad082?q=80&w=2276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "Branislav Rodman",
+    title: "A Black and White Photo of a Woman Brushing Her Teeth",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1640680608781-2e4199dd1579?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/a-painting-of-a-palm-leaf-on-a-multicolored-background-AaNPwrSNOFE",
+    title: "Neon Palm",
+    author: "Tim Mossholder",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1726083085160-feeb4e1e5b00?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/a-blurry-photo-of-a-crowd-of-people-UgbxzloNGsc",
+    author: "ANDRII SOLOK",
+    title: "A blurry photo of a crowd of people",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1562016600-ece13e8ba570?q=80&w=2838&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/rippling-crystal-blue-water-9-OCsKoyQlk",
+    author: "Wesley Tingey",
+    title: "Rippling Crystal Blue Water",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1624344965199-ed40391d20f2?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/de/fotos/mann-im-schwarzen-hemd-unter-blauem-himmel-m8RDNiuEXro",
+    author: "Serhii Tyaglovsky",
+    title: "Mann im schwarzen Hemd unter blauem Himmel",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1689553079282-45df1b35741b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/a-woman-with-a-flower-crown-on-her-head-0S3muIttbsY",
+    author: "Vladimir Yelizarov",
+    title: "A women with a flower crown on her head",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1721968317938-cf8c60fccd1a?q=80&w=2728&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "A blurry photo of white flowers in a field",
+    author: "Eugene Golovesov",
+    link: "https://unsplash.com/photos/a-blurry-photo-of-white-flowers-in-a-field-6qbx0lzGPyc",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1677338354108-223e807fb1bd?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "Mathilde Langevin",
+    link: "https://unsplash.com/photos/a-table-topped-with-two-wine-glasses-and-plates-Ig0gRAHspV0",
+    title: "A table topped with two wine glasses and plates",
+  },
+]
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center">
+      <Floating sensitivity={-0.5} className="h-screen hidden md:block">
+        <FloatingElement
+          depth={0.5}
+          className="top-[15%] left-[2%] md:top-[25%] md:left-[5%]"
+        >
+          <motion.img
+            src={exampleImages[0].url}
+            alt={exampleImages[0].title}
+            className="w-16 h-12 sm:w-24 sm:h-16 md:w-28 md:h-20 lg:w-32 lg:h-24 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform -rotate-[3deg] shadow-2xl rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+          />
+        </FloatingElement>
+
+        <FloatingElement
+          depth={1}
+          className="top-[0%] left-[8%] md:top-[6%] md:left-[11%]"
+        >
+          <motion.img
+            src={exampleImages[1].url}
+            alt={exampleImages[1].title}
+            className="w-40 h-28 sm:w-48 sm:h-36 md:w-56 md:h-44 lg:w-60 lg:h-48 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform -rotate-12 shadow-2xl rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.7 }}
+          />
+        </FloatingElement>
+
+        <FloatingElement
+          depth={4}
+          className="top-[90%] left-[6%] md:top-[80%] md:left-[8%]"
+        >
+          <motion.img
+            src={exampleImages[2].url}
+            alt={exampleImages[2].title}
+            className="w-40 h-40 sm:w-48 sm:h-48 md:w-60 md:h-60 lg:w-64 lg:h-64 object-cover -rotate-[4deg] hover:scale-105 duration-200 cursor-pointer transition-transform shadow-2xl rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.9 }}
+          />
+        </FloatingElement>
+
+        <FloatingElement
+          depth={2}
+          className="top-[0%] left-[87%] md:top-[2%] md:left-[83%]"
+        >
+          <motion.img
+            src={exampleImages[3].url}
+            alt={exampleImages[3].title}
+            className="w-40 h-36 sm:w-48 sm:h-44 md:w-60 md:h-52 lg:w-64 lg:h-56 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform shadow-2xl rotate-[6deg] rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.1 }}
+          />
+        </FloatingElement>
+
+        <FloatingElement
+          depth={1}
+          className="top-[78%] left-[83%] md:top-[68%] md:left-[83%]"
+        >
+          <motion.img
+            src={exampleImages[4].url}
+            alt={exampleImages[4].title}
+            className="w-44 h-44 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform shadow-2xl rotate-[19deg] rounded-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.3 }}
+          />
+        </FloatingElement>
+      </Floating>
+
+      <div className="flex flex-col items-center justify-center gap-8 mt-20 md:mt-32">
+        <HeroBadge
+          href="/signup"
+          text="Become an Instructor"
+          icon={<User className="h-4 w-4" />}
+          endIcon={<ChevronRight className="h-4 w-4" />}
+        />
+
+        <h1 className="text-center bg-gradient-to-br dark:from-white from-black from-30% dark:to-white/40 to-black/40 bg-clip-text text-5xl font-medium text-transparent text-balance md:text-7xl z-50">
+          Unlock Your Potential with In-Demand{" "}
+          <HighlightText text={"Coding Skills"} />
+        </h1>
+
+        <p className="text-lg md:text-xl text-balance text-center text-neutral-400 tracking-wide z-50">
+          Learn to code at your own pace, anywhere, with access to hands-on
+          projects, quizzes, and personalized feedback from expert instructors.
+        </p>
+
+        <div className="flex md:flex-row flex-col gap-7 z-50">
+          <Button asChild variant={"default"}>
+            <Link href="/signup">Learn More</Link>
+          </Button>
+          <Button asChild variant={"outline"}>
+            <Link href="/signup">Book a Demo</Link>
+          </Button>
+        </div>
+      </div>
+
+      <video
+        className="mx-3 mt-28 rounded-3xl relative w-[80%] p-3 bg-white/15 backdrop-blur-md" 
+        muted
+        loop
+        autoPlay
+      >
+        <source src="/images/banner.mp4" type="video/mp4" />
+      </video>
+    </div>
+  )
+}
