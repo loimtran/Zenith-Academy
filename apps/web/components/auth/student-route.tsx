843af508b08f2f1ef5c448019dc38ab8ce4895bf
loimtran <loi.tran.dev@gmail.com>
2025-02-14 01:08:51 +0530

    feat: add StudentRoute component to restrict access for non-student users

---
+++ apps/web/components/auth/student-route.tsx
@@ -0,0 +1,28 @@
+// This will prevent non Student users from accessing this route
+"use client"
+
+import { useEffect } from "react"
+import { notFound, useRouter } from "next/navigation"
+import { ACCOUNT_TYPE } from "@/data/constants"
+import { useProfileStore } from "@/store/use-profile-store"
+
+import Loading from "@/app/loading"
+
+export default function StudentRoute({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { user, loading } = useProfileStore()
+  const isStudent = user?.accountType === ACCOUNT_TYPE.STUDENT
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isStudent && !loading) {
+      return notFound()
+    }
+  }, [user?.accountType, loading, router])
+
+  if (isStudent) return children
+  else return <Loading />
+}
