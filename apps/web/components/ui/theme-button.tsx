c7bfa2c0ee3959c195ba1609fd39ea531e94bbcf
loimtran <loi.tran.dev@gmail.com>
2025-09-14 01:04:51 +0530

    refactor: update z-index values and padding for dropdown menu, sheet overlay, and theme buttons; add new aboutus2 image asset

--- apps/web/components/ui/theme-button.tsx
+++ apps/web/components/ui/theme-button.tsx
@@ -5,7 +5,7 @@ export function MainButton({ children }: { children: React.ReactNode }) {
   return (
     <Button
       variant={"default"}
-      className="group flex cursor-pointer text-white items-center bg-blue-600 px-8 py-3 whitespace-nowrap leading-[1.7em] transition-opacity duration-300 hover:opacity-80 space-x-2 hover:bg-blue-700 hover:shadow-lg"
+      className="group flex cursor-pointer text-white items-center bg-blue-600 px-8 py-7 whitespace-nowrap leading-[1.7em] transition-opacity duration-300 hover:opacity-80 space-x-2 hover:bg-blue-700 hover:shadow-lg"
     >
       <p className="font-semibold text-lg text-white transition-all duration-300 group-hover:-translate-x-1 group-hover:text-ocean mr-2">
         {children}
@@ -20,7 +20,7 @@ export function ButtonOutline({ children }: { children: React.ReactNode }) {
     <Button
       type="button"
       variant="outline"
-      className=" border-2 border-blue-400 bg-transparent px-8 py-3 text-lg font-semibold text-blue-400 transition-all duration-300 ease-out hover:bg-blue-900/30"
+      className=" border-2 border-blue-400 bg-transparent px-8 py-7 text-lg font-semibold text-blue-400 transition-all duration-300 ease-out hover:bg-blue-900/30"
     >
       {children}
     </Button>
